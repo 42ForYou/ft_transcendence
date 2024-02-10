ac210b0dbee3079224020c93b98a680d0dd01b16
yonaMac <yonaimyo@gmail.com>
2024-02-10 15:10:16 +0900

    build(docker): nginx 컨테이너 entrypoint 스크립트 작성 및 적용
    - .env 파일을 수정했음에도 환경 변수 세팅이 안되어서 일단 도커파일 내에 하드코딩함

---
+++ src/nginx/scripts/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "what the fuck"
+echo $BACKEND_HOST $BACKEND_PORT
+
+# backend 서비스가 준비될 때까지 대기
+/scripts/wait-for-backend.sh $BACKEND_HOST $BACKEND_PORT -- 
+
+# nginx 실행
+exec nginx -g 'daemon off;'
