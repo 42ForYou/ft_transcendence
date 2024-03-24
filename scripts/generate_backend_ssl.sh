70a4f212653af0a34bae0a6021fb965f6408544b
JinMuGo <jingmuio@gmail.com>
2024-03-24 17:42:59 +0900

    refactor(ssl-init): separate to Dockerfile

--- scripts/generate_backend_ssl.sh
+++ scripts/generate_backend_ssl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 openssl req -x509 -nodes -days 365 \
     -subj "/C=KR/L=Seoul/O=42Seoul/CN=42seoul" \
     -newkey rsa:2048 \
--- scripts/generate_nginx_ssl.sh
+++ scripts/generate_nginx_ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 openssl req -x509 -nodes -days 365 \
     -subj "/C=KR/L=Seoul/O=42Seoul/CN=42foryou" \
     -newkey rsa:2048 \
